486c9071ffdd6ed3a759986cd37ea9f8dea511b8
qrk1 <pgmelton@gmail.com>
2018-05-29 22:51:04 -0400

    End of Day May 29, 2018

--- ex20.py
+++ ex20.py
@@ -1,15 +1,32 @@
 # Imports the argv function from the sys module
 from sys import argv
-
+# Takes the passed values from when the program was called
+# and assigns the prgram name to scripts and the passed
+# file name to input_file.
 script, input_file = argv
 
+# Defines a functions called print_all which passed the
+# parameter f. The parameter that is passed to the function
+# is unique to the function. When called you would code
+# print_all(input_file), input_file would be passed and
+# used as f in the function.
 def print_all(f):
+    # Prints f (the file) with the read command.
     print(f.read())
 
+# Defines a function called rewind. and passes a parameter, f.
 def rewind(f):
+    # Applies the seek command to the parameter (file)
+    # that is passed into the function. The seek() method
+    # sets the file current position. In this case will
+    # go to the first position, "0".
     f.seek(0)
 
+# Defines the function print_a_line and passes two
+# parameters, line_count and f.
 def print_a_line(line_count, f):
+    # Will print the line_count and will read a line
+    # in the file f (will go to EOF, since nothing is in
     print(line_count, f.readline())
 
 current_file = open(input_file)
--- ex21.py
+++ ex21.py
@@ -1,4 +1,33 @@
-# this will be my 21st exercise
-from sys imoort argv
+def add(a, b):
+    print(f"ADDING {a} + {b}")
+    return a + b
 
-script, new_file = agrv
+def subtract (a, b):
+    print(f"SUBTRACTING {a} - {b}")
+    return a - b
+
+def multiply(a, b):
+    print(f"MULTIPLYING {a} * {b}")
+    return a * b
+
+def divide (a, b):
+    print(f"DIVIDING {a} / {b}")
+    return a / b
+
+
+print("Let's do some math with just functions!")
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}")
+
+
+# A puzzle for the extra credit, type it in anyway.
+print("Here is a puzzle.")
+
+what = add(age, subtract(height, multiply(weight, divide(1,2))))
+
+print("That becomes: ", what, "Can you do it by hand?")
